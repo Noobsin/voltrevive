2c42499571bf74ec470776703b0b6d33d85e630c
sakibsadman928 <sakibsadman928@gmail.com>
2026-05-03 22:00:15 +0600

    Fix login route in web.php

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::get('/technicians/marcus/portfolio', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/register',  [AuthController::class, 'showRegister']);
     Route::post('/register', [AuthController::class, 'register']);
-    Route::get('/login',     [AuthController::class, 'showLogin']);
+    Route::get('/login',     [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login',    [AuthController::class, 'login']);
 });
 
